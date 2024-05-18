cf63c3745bb9ff7b56b9064bcd20510de5df8042
CoconutBin <sea99687@gmail.com>
2024-05-18 21:39:59 +0700

    Fixed Issue with Edit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ if (!typeof (Storage)) {
     alert("Your browser does not support local storage, so list items won't save when you exit the tab")
 }
 
-if (storedlistContents && storedlistContents != []) {
+if (storedlistContents && storedlistContents.length > 0) {
     for (let i = 0; i < storedlistContents.length; i++) {
         addListItem(storedlistContents[i])
     }
@@ -17,42 +17,38 @@ if (storedlistContents && storedlistContents != []) {
 inputDiv.addEventListener(
     "submit", function (event) {
         event.preventDefault();
-        if (!input.value || input.value == "") {
-        }
-        else {
+        if (input.value) {
             addListItem(input.value)
-            input.value = "" //Clears text field
+            input.value = "" 
         }
-
     }
 )
 
 function addListItem(input) {
-    const child = document.createElement("h1");
+    const child = document.createElement("h1"); 
     const deleteButton = document.createElement("input")
     const editButton = document.createElement("input")
     const editModal = document.createElement("div")
 
-    //Adds list item
-    child.classList.add("listItem")  
+    // Adds list item
+    child.classList.add("listItem")
     child.textContent = input;
     list.appendChild(child);
 
-    //Adds delete button
+    // Adds delete button
     deleteButton.type = "button"
-    deleteButton.classList.add("button") 
+    deleteButton.classList.add("button")
     deleteButton.value = "Delete"
     deleteButton.addEventListener(
         "click", function () {
-            const deletedListContent = deleteButton.parentElement.textContent
+            const deletedListContent = child.textContent; 
             listContents.splice(listContents.indexOf(deletedListContent), 1)
-            deleteButton.parentElement.remove()
+            child.remove()
             if (listContents) { localStorage.setItem("listContents", JSON.stringify(listContents)) }
         }
-
     )
 
-    //Adds edit modal
+    // Adds edit modal
     const editField = document.createElement("input")
     const editFieldSubmit = document.createElement("input")
     const editForm = document.createElement("form")
@@ -66,31 +62,27 @@ function addListItem(input) {
     editForm.addEventListener(
         "submit", function (event) {
             event.preventDefault();
-            if (!editField.value || editField.value == "") {
-            }
-            else {
-                const editedListContent = editButton.parentElement.textContent
+            if (editField.value) {
+                const editedListContent = child.textContent; 
                 child.textContent = editField.value
-                editModal.style.display = 'hidden'
-                console.log(listContents
-                )
-                console.log(editedListContent)
-                console.log(listContents.indexOf(editedListContent))
+                child.appendChild(deleteButton)
+                child.appendChild(editButton)
+                child.appendChild(editModal)
+                editModal.style.display = 'none'
                 listContents.splice(listContents.indexOf(editedListContent), 1, editField.value)
                 if (listContents) { localStorage.setItem("listContents", JSON.stringify(listContents)) }
             }
-    
         }
     )
     editModal.appendChild(editForm)
 
-    //Adds edit button
+    // Adds edit button
     editButton.type = "button"
-    editButton.classList.add("button") 
+    editButton.classList.add("button")
     editButton.value = "Edit"
     editButton.addEventListener(
-        "click", function() {
-            editModal.style.display = "block"
+        "click", function () {
+            editModal.style.display = "flex"
         }
     )
 
