06d3022a327d0d4bc2ab776c4bc4757d07be12e8
CoconutBin <sea99687@gmail.com>
2024-07-14 01:36:42 +0700

    version change

--- src/js/localStorage.js
+++ src/js/localStorage.js
@@ -1,4 +1,4 @@
-const version = "2.4";
+const version = "2.5";
 if (localStorage.getItem("version") !== null && localStorage.getItem("version") !== version) {
     if (parseInt(localStorage.getItem("version").split(".")[0]) > 1) {
         console.log("currently running version " + version);
