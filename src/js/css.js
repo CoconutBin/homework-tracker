fbae9dba48ceb60139b104c00ba7154428f3e2ac
CoconutBin <sea99687@gmail.com>
2024-07-25 21:53:46 +0700

    redid scaling

--- src/js/css.js
+++ src/js/css.js
@@ -1,6 +1,6 @@
 function setItemListWidth(){
-    document.querySelector(':root').style.setProperty('--listItem-width', `${(window.innerWidth/Math.round(window.innerWidth/375))-20}px`);
+    document.querySelector(':root').style.setProperty('--listItem-width', `${(window.innerWidth/Math.round(window.innerWidth/375))-40}px`);
 }
 
-
+setItemListWidth()
 window.onresize = setItemListWidth;
