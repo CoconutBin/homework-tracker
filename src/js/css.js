ca595f2f6cb9390b1cec6e7fd2e343ca3d015766
CoconutBin <sea99687@gmail.com>
2024-07-25 22:04:45 +0700

    small style changes

--- src/js/css.js
+++ src/js/css.js
@@ -1,5 +1,11 @@
 function setItemListWidth(){
     document.querySelector(':root').style.setProperty('--listItem-width', `${(window.innerWidth/Math.round(window.innerWidth/375))-40}px`);
+    if(window.innerWidth < 385){
+        document.getElementById('logo').innerText = 'wrkd.'
+    } else {
+        document.getElementById('logo').innerText = 'Homework Tracker'
+    }
+    
 }
 
 setItemListWidth()
