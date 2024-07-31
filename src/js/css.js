894ce2d45bc159e4c97f6d40723b14cffde0755e
CoconutBin <sea99687@gmail.com>
2024-07-31 08:17:36 +0700

    Fixed Issue with Navbar Displaying

--- src/js/css.js
+++ src/js/css.js
@@ -1,11 +1,13 @@
 function setItemListWidth(){
     document.querySelector(':root').style.setProperty('--listItem-width', `${(window.innerWidth/Math.round(window.innerWidth/375))-40}px`);
+
     if(window.innerWidth < 385){
         document.getElementById('logo').innerText = 'wrkd.'
-    } else {
+    } else if(document.querySelector('title').value == 'Homework Tracker'){
         document.getElementById('logo').innerText = 'Homework Tracker'
-    }
-    
+    } else {
+            document.getElementById('logo').innerText = 'Archived Homeworks'
+        }
 }
 
 setItemListWidth()
