1d0c62fadb5087425110419c1c21ebb01c999fb8
CoconutBin <sea99687@gmail.com>
2024-09-25 00:14:54 +0700

    version update

--- src/ts/localStorage.ts
+++ src/ts/localStorage.ts
@@ -1,4 +1,4 @@
-const version: string = "2.6"
+const version: string = "2.7"
 
 if (localStorage.getItem("version") !== null && localStorage.getItem("version") !== version) {
     if(parseInt(localStorage.getItem("version").split(".")[0]) > 1) {
